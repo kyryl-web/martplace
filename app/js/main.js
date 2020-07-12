a4a77adbc795375a42a2598914b065041c054e54
kyryl-web <kkimarski@gmail.com>
2020-07-12 20:44:25 +0300

    fixed resume slider

--- app/js/main.js
+++ app/js/main.js
@@ -18,8 +18,8 @@ $(function(){
     });
 
       $('.products__resume').slick({
-        slidesToShow: 2,
-        slidesToScroll: 2,
+        slidesToShow: 1,
+        slidesToScroll: 1,
         prevArrow: '<button class="slick-arrow arrow-prev"><img src="images/arrow-prev.png" alt=""></button>',
         nextArrow: '<button class="slick-arrow arrow-next"><img src="images/arrow-next.png" alt=""></button>'
       });
